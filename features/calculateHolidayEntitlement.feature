77b9234c033b0b3e24f94ccda03036d8573846e6
erclaudio <claudio.andrei@icloud.com>
2024-07-30 10:56:43 +0100

    Added scenarios (excluding errors)

--- features/calculateHolidayEntitlement.feature
+++ features/calculateHolidayEntitlement.feature
@@ -17,10 +17,27 @@ Scenario: Calculate the correct holiday entitlement for an employee who works re
     And I should see the total entitlement hours
 
 Scenario: Calculate the correct holiday entitlement for an employee who works irregular hours
+    Given I navigate to the homepage
+    And I should see the homepage
+    When I click on the 'Start now' button 
+    And I select the option yes for working irregular hours
+    And I input 10 October 2024 for the the date the leave starts
+    And I input 375 work hours for the hours worked in the pay period
+    Then I should see the correct total entitlement hours
 
 
-Scenario: Displays the values that were inputted by the user on the summary page
 
-Scenario: Displays error when the user does not select a mandatory field
 
 Scenario: Updates calculation when value is edited
+    Given I navigate to the homepage
+    And I should see the homepage
+    When I click on the 'Start now' button 
+    And I select the option yes for working irregular hours
+    And I input 10 October 2024 for the the date the leave starts
+    And I input 375 work hours for the hours worked in the pay period
+    And I should see the correct total entitlement hours
+    When I click 'change' for the hours worked in the pay period
+    And I input 450 hours
+    Then I should see the new correct total entitlement hours
+
+
--- features/step_definitions/calculate_holiday_entitlement_steps.rb
+++ features/step_definitions/calculate_holiday_entitlement_steps.rb
@@ -1,5 +1,5 @@
 response_hours, response_days = 37.5, 5.0
-
+# SCENARIO HOMEPAGE IS VSIBLE
 Given('I navigate to the homepage') do 
     visit 'https://www.gov.uk/calculate-your-holiday-entitlement'
     sleep 1
@@ -11,12 +11,12 @@ And('I should see the homepage') do
     expect(page).to have_link 'Start now'
     expect(page).to have_content 'Use this tool to calculate holiday entitlement for:'
 end
-
+# SCENARIO CALCULATE THE CORRECT HOLIDAY ENTINLEMENT FOR AN EMPLOYEE WHO WORKS REGULAR HOURS
 When ("I click on the 'Start now' button") do
     click_link 'Start now'
 end
 
-And("I select the option no for working irregular hours") do
+And('I select the option no for working irregular hours') do
     choose('No', allow_label_click: true)
     click_button 'Continue'
 end
@@ -52,14 +52,62 @@ Then ('I should see the correct submitted answers') do
         expect(page).to have_css('dd.govuk-summary-list__value', text: '37.5')
         expect(page).to have_css('dd.govuk-summary-list__value', text: '5.0')
     end
-    sleep 20
+    sleep 1
 
     
 end
 
 And ('I should see the total entitlement hours') do
     expect(page).to have_content '210'
-    sleep 20
+    sleep 1
     
     
+end
+
+# SCENARIO Calculate the correct holiday entitlement for an employee who works irregular hours
+
+
+
+And('I select the option yes for working irregular hours') do
+    choose('Yes', allow_label_click: true)
+    click_button 'Continue'
+end
+
+And ('I input 10 October 2024 for the the date the leave starts') do
+    fill_in 'response-0', with: '10'
+    fill_in 'response-1', with: '10'
+    fill_in 'response-2', with: '2024'
+    click_button 'Continue'
+end
+     
+
+And ('I input 375 work hours for the hours worked in the pay period') do
+    fill_in 'response', with: '375'
+    click_button 'Continue'
+end
+
+Then ('I should see the correct total entitlement hours') do
+    expect(page).to have_content 'The statutory entitlement for this pay period is 45 hours.'
+    sleep 1
+    
+    
+end
+
+# SCENARIO UPDATES CALCULATIONS WHEN VALUE IS UPDATED
+When ("I click 'change' for the hours worked in the pay period") do
+    # click_link('a[href="/calculate-your-holiday-entitlement/y/irregular-hours-and-part-year/2024-10-10?previous_response=375.0"]')
+    find('a[data-module="ga4-link-tracker"][href="/calculate-your-holiday-entitlement/y/irregular-hours-and-part-year/2024-10-10?previous_response=375.0"]').click
+
+end
+
+
+And ('I input 450 hours')do
+    fill_in 'response', with:'450'
+    click_button 'Continue'
+end
+
+Then ('I should see the new correct total entitlement hours')do
+    expect(page).to have_content 'The statutory entitlement for this pay period is 54 hours.'
+    sleep 3
+
 end
